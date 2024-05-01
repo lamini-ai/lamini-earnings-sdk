6843fd6bd109bf5f2b7143f0490dbbad236558cc
Gregory Diamos <gregorydiamos@MacBook-Air-Gregory.local>
2024-05-01 07:14:22 -0600

    readme updates

---
+++ 03_prompt_tuning/README.md
@@ -0,0 +1,16 @@
+<div align="center">
+<img src="https://avatars.githubusercontent.com/u/130713213?s=200&v=4" width="110"><img src="https://huggingface.co/lamini/instruct-peft-tuned-12b/resolve/main/Lamini_logo.png?max-height=110" height="110">
+</div>
+
+# Prompt Tuning
+
+This video is a tutorial of how to prompt tune Open LLMs: https://www.youtube.com/watch?v=f32dc5M2Mn0
+
+Run Llama 3 on a few example questions:
+
+```bash
+./03_prompt_tuning/scripts/spot_check.sh
+```
+
+
+
--- 06_fine_tuning/train.py
+++ 06_fine_tuning/train.py
@@ -4,7 +4,10 @@ import jsonlines
 
 def main():
     llm = Lamini(model_name="meta-llama/Meta-Llama-3-8B-Instruct")
-    llm.train(data=load_training_data())
+
+    dataset = list(load_training_data()) * 30
+
+    llm.train(data=dataset)
 
 def load_training_data():
     path = "/app/lamini-earnings-sdk/data/generated_q_a.jsonl"
@@ -20,10 +23,13 @@ def load_training_data():
             header_end = "<|eot_id|><|start_header_id|>assistant<|end_header_id|>"
 
             yield {
-                "input": header + obj["question"] + header_end,
+                "input": header + make_question(obj) + header_end,
                 "output": obj["answer"] + "<|eot_id|>",
             }
 
+def make_question(obj):
+    question = f"Consider the following company: {obj['ticker']}) and quarter: {obj['q']}. "
+    question += obj["question"]
+    return question
+
 main()
-
-
